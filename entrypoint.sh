275df99f640d0f0f685ba24e85d9c2a2fafca86e
= <=>
2026-09-25 10:47:03 +0000

    Use a2dismod to cleanly disable mpm_event and mpm_worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-# Désactiver les MPM thread-safe (event et worker) pour éviter le crash avec le module PHP non thread-safe
-# On supprime physiquement les fichiers de chargement pour éviter tout conflit
-rm -f /etc/apache2/mods-enabled/mpm_event.load
-rm -f /etc/apache2/mods-enabled/mpm_worker.load
+# Désactiver proprement les modules MPM thread-safe et leurs configurations
+a2dismod mpm_event || true
+a2dismod mpm_worker || true
+
+# Activer mpm_prefork qui est compatible avec le module PHP non thread-safe
 a2enmod mpm_prefork
 
 # Lancer Apache au premier plan
