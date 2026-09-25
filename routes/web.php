197ee691dc260213099d031c3a0b696543a92928
= <=>
2026-09-25 13:00:28 +0000

    Allow both super-admin and Administrateur roles for admin access

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/test-final-99', 'App\Http\Controllers\AdminController@inscription')
     Route::get('/restauration', 'App\Http\Controllers\AdminController@restauration')->name('restauration');
     Route::post('/commander', 'App\Http\Controllers\AdminController@getCommandes')->name('get.commande');
 
-    Route::group(['middleware' => ['auth', 'role:super-admin']], function () {
+    Route::group(['middleware' => ['auth', 'role:super-admin|Administrateur']], function () {
         Route::get('/admin', function() {
             return redirect()->route('Admin.parametres.index');
         })->name('admin');
@@ -38,6 +38,7 @@ Route::get('/test-final-99', 'App\Http\Controllers\AdminController@inscription')
         Route::get('/admin/impression',  'App\Http\Controllers\AdminController@impression')->name('Admin.groupe.impression');
         Route::get('/admin/restauration',  'App\Http\Controllers\AdminController@gestionRestaurant')->name('Admin.restauration');
         Route::get('/admin/etudiants', 'App\Http\Controllers\AdminController@participantAddView')->name('Admin.participantAdd');
+
 
 
         Route::get('/pdf/listeEquipe/niveau1', 'App\Http\Controllers\pdfController@listeEquipeN1')->name('liste.equipe.n1');
